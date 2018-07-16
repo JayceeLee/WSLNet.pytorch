e90e870d3f55aea6504d17de70e8719155d9912f
Mayouji <llcing@outlook.com>
2018-07-16 13:21:31 +0800

    wild_cat

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ DATA_ROOT=/home/lucliu/dataset
 DATA=voc # mscoco, nus-wide, wider 
 DATA_PATH=${DATA_ROOT}/${DATA}
 
-BASE_MODEL=ours # ours, baseline, wildcat
+BASE_MODEL=wildcat # ours, baseline, wildcat
 EXTEND_MODEL=50 # 50, 101
 MODEL=${BASE_MODEL}_${EXTEND_MODEL}
 
