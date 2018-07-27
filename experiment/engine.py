2c44d68aa0490bffe198dec34797aec8ac0441ce
Mayouji <llcing@outlook.com>
2018-07-27 15:14:46 +0800

    back

--- experiment/engine.py
+++ experiment/engine.py
@@ -54,8 +54,7 @@ class Engine(object):
         self.state['data_time'] = tnt.meter.AverageValueMeter()
         # display parameters
         if self._state('use_pb') is None:
-            # self.state['use_pb'] = True
-            self.state['use_pb'] = False
+            self.state['use_pb'] = True
         if self._state('print_freq') is None:
             self.state['print_freq'] = 0
 
